1cd1ed2ba66f1f8a6b4f70f2f8662890450f73cb
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2020-11-09 22:11:12 +0300

    Add HelloController.cs

---
+++ SearchApi/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchApi.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string GetHello()
+        {
+            return "hello";
+        }
+    }
+}
