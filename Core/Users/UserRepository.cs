b365057754ae8033686dfbd89d8c1b0dc0342383
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2020-12-18 22:33:33 +0300

    Final user repo

--- Core/Users/UserRepository.cs
+++ Core/Users/UserRepository.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Models;
 using Core.Storage;
+using Newtonsoft.Json;
 
 namespace Core.Users
 {
@@ -19,27 +21,37 @@ namespace Core.Users
         {
             var path = Path + userModel.UserName + ".json";
             if(!File.Exists(path))
-                
+                File.Create(path).Close();
+
+            await FileOperations.WriteObjectToFile(path, userModel);
         }
 
         public async Task<List<UserModel>> GetUsers()
         {
-            
+            var docs = Directory.GetFiles(Path);
+            var result = docs.Select(doc => 
+                JsonConvert.DeserializeObject<UserModel>(File.ReadAllText(doc))).ToList();
+            return result;
         }
 
         public async Task<UserModel> GetUser(string userName)
         {
-            
+            var path = Path + userName + ".json";
+            return File.Exists(path)
+                ? JsonConvert.DeserializeObject<UserModel>(await File.ReadAllTextAsync(path))
+                : null;
         }
 
         public async Task UpdateUser(string userName, UserModel userModel)
         {
-            
+            var path = Path + userName + ".json";
+            await FileOperations.WriteObjectToFile(path, userModel);
         }
 
         public async Task DeleteUser(string userName)
         {
-            
+            var path = Path + userName + ".json";
+            await FileOperations.DeleteFile(path);
         }
     }
 }
